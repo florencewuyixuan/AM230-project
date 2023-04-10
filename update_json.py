1961ef3e8d7f783e6dac5633046359fac0f20898
FWu <yxw@umich.edu>
2023-04-09 21:23:21 -0400

    update velocity

--- update_json.py
+++ update_json.py
@@ -4,6 +4,6 @@ def update_json(filename, velocity):
     jsonData = json.load(open(filename, "r"))
     num = len(velocity)
     for i in range(num):
-        jsonData['system']['particles'][i]["v"] = velocity[i]
+        jsonData['system']['particles'][i]["v"] = velocity[i]*jsonData['system']['particles'][i]["n"]
 
     json.dump(jsonData, open(filename ,"w"), indent=4)
