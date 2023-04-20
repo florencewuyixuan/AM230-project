906886772bf18daa31bce884f85fb20906da7369
FWu <yxw@umich.edu>
2023-04-19 23:48:41 -0400

    update alpha to a vector

---
+++ cuda/gpumd/md/build/CMakeFiles/nvccmodule.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/florence/Desktop/AM230/ABPTutorial/cuda/gpumd/md/src/from_json.cpp" "CMakeFiles/nvccmodule.dir/src/from_json.cpp.o" "gcc" "CMakeFiles/nvccmodule.dir/src/from_json.cpp.o.d"
+  "/home/florence/Desktop/AM230/ABPTutorial/cuda/gpumd/md/src/pybindexport.cpp" "CMakeFiles/nvccmodule.dir/src/pybindexport.cpp.o" "gcc" "CMakeFiles/nvccmodule.dir/src/pybindexport.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
